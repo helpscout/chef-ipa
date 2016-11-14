6eb3d1252f6ad708446c001c9fc4d54e60ee2c6a
Jared Baldridge <jrb@expunge.us>
2016-11-14 11:55:57 -0500

    Adding urls

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description       'A cookbook for managing IPA installations.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.1.2'
 
+issues_url        'https://github.com/helpscout/chef-ipa/issues'
+source_url        'https://github.com/helpscout/chef-ipa'
+
 supports          'ubuntu', '>= 14.04'
 
 # vim: ai ts=2 sts=2 et sw=2 ft=ruby
