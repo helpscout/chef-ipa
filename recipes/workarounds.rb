c118eb48f0479f30e52155eee1614946c412d2f6
Jason Ramey <jasonramey@helpscout.com>
2017-07-25 14:57:31 -0400

    replacing incorrect unless for if

--- recipes/workarounds.rb
+++ recipes/workarounds.rb
@@ -30,7 +30,7 @@ when 'ubuntu', 'debian'
   end
 
 # sudo
-  unless node['ipa']['client']['sudo']
+  if node['ipa']['client']['sudo']
     bash 'Enable SSSD sudo support' do
       code "sed -i -e 's/^services =.*$/services = nss, sudo, pam, ssh/g' /etc/sssd/sssd.conf"
       not_if 'grep sudo /etc/sssd/sssd.conf'
